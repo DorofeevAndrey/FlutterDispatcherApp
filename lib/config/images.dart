17d41af9701a17166878cad54e0de534c1e24ee6
Andrey <dorofeevandrey78@gmail.com>
2025-04-08 20:02:25 +0500

    firstAppBar

---
+++ lib/config/images.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_svg/flutter_svg.dart';
+
+final SvgPicture accountCircleImage = SvgPicture.asset(
+  'assets/svg/account_circle.svg',
+);
+
+final SvgPicture arrowBackImage = SvgPicture.asset('assets/svg/arrow_back.svg');
+
+final SvgPicture checkImage = SvgPicture.asset('assets/svg/check.svg');
+
+final SvgPicture motorcycleImage = SvgPicture.asset(
+  'assets/svg/motorcycle.svg',
+);
+
+final SvgPicture stateBoardingImage = SvgPicture.asset(
+  'assets/svg/state_boarding.svg',
+);
+
+final SvgPicture stateDeliveryImage = SvgPicture.asset(
+  'assets/svg/state_delivery.svg',
+);
+final SvgPicture stateLoadImage = SvgPicture.asset('assets/svg/state_load.svg');
+final SvgPicture stateParkingImage = SvgPicture.asset(
+  'assets/svg/state_parking.svg',
+);
+final SvgPicture statePickupImage = SvgPicture.asset(
+  'assets/svg/state_pickup.svg',
+);
+final SvgPicture stateRepairImage = SvgPicture.asset(
+  'assets/svg/state_repair.svg',
+);
+final SvgPicture stateUnboardingImage = SvgPicture.asset(
+  'assets/svg/state_unboarding.svg',
+);
+final SvgPicture stateUnloadImage = SvgPicture.asset(
+  'assets/svg/state_unload.svg',
+);
+
+final SvgPicture busImage = SvgPicture.asset('assets/svg/bus.svg');
+
+final SvgPicture carImage = SvgPicture.asset('assets/svg/car.svg');
+
+final SvgPicture truckImage = SvgPicture.asset('assets/svg/truck.svg');
+
+final SvgPicture tuktukImage = SvgPicture.asset('assets/svg/tuktuk.svg');
---
+++ lib/pages/vehicle/vehicle_page.dart
@@ -0,0 +1,27 @@
+import 'package:dispatcherapp/config/colors.dart';
+import 'package:dispatcherapp/config/dimensions.dart';
+import 'package:flutter/material.dart';
+
+class VehiclePage extends StatelessWidget {
+  const VehiclePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Kiparo.com Dispatcher',
+          style: TextStyle(
+            color: primaryColor,
+            fontSize: fontSize16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: surfaceyColor,
+        elevation: 0,
+      ),
+      body: Container(color: backgroundColor, child: const Text('Body')),
+    );
+  }
+}
